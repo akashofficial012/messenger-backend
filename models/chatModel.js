ee9a154d904330dd7068beee02436d44a74b9fa7
akashofficial012 <akashmondal.code@gmail.com>
2025-05-15 18:35:47 +0600

    all model create

---
+++ models/chatModel.js
@@ -0,0 +1,32 @@
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const chatSchema = new Schema({
+  
+    name: {
+      type: String,
+      required: true,
+    },
+    groupChat: {
+      type: Boolean,
+      default: false,
+    },
+    creator: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+    members: [
+      {
+        type: Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Chat = mongoose.model('Chat', chatSchema);
